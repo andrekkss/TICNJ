38bb58c92c3000e656cb9c93633bce615161a12b
André <andre.kitano.silva@gmail.com>
2018-10-19 21:54:01 -0300

    javadoc e gerenciamento da list

--- src/model/List.java
+++ src/model/List.java
@@ -6,19 +6,43 @@
 package model;
 
 import java.util.*;
-/**
- *
- * @author andre
- */
-public class List<T> {
-    ArrayList<T> list = new ArrayList<T>();
-    
-    public List (){}
 
+/**
+ * Essa classe para gerenciamento da ArrayList no qual irá receber os parametros dinamicamente
+ * @author andre
+ * @version 1.0
+ * @since 19-10-2018
+ */
+public class List<T>{
+    ArrayList<T> list = new ArrayList<T>();
+
+    /**
+     * Essa função faz a inserção da lista
+     * @param t O parametro do tipo Object
+     */
     public void isInsert(T t){ 
         list.add(t); 
     }
     
+    /**
+     * IsList ira listar o conteudo da lista
+     */
+    public void isList(){
+        for(T t: list){
+            System.out.println(t);
+        }
+    }
+    /**
+     * Essa função faz o delete de um dado na lista
+     * @param t O parametro do tipo Object
+     */
+    public void isDelete(T t){
+        list.remove(t);
+    }
+    
+    /**
+     * Essa função checa se a lista ta vazia
+     */
     public void isEmpty(){ 
         if(list.isEmpty()){
             System.out.println("Lista está vazia");
