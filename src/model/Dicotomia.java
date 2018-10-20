0859b695f0c9d2929dd768ad1d30db2b5e9e3d4d
Caique <Caique@Caique>
2018-10-19 22:29:22 -0300

    Docotomia Getters/Setters e JavaDoc

--- src/model/Dicotomia.java
+++ src/model/Dicotomia.java
@@ -6,9 +6,43 @@
 package model;
 
 /**
- *
+ * Classe responsável por fazer as equações referentes a Dicotomia 
+ * @since 19-10-2018
  * @author andre
  */
 public class Dicotomia {
+    
+    private double x1;
+    private double x2;
+
+    /**
+     * @return the x1
+     */
+    public double getX1() {
+        return x1;
+    }
+
+    /**
+     * @param x1 the x1 to set
+     */
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    /**
+     * @return the x2
+     */
+    public double getX2() {
+        return x2;
+    }
+
+    /**
+     * @param x2 the x2 to set
+     */
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    
     
 }
