17b78f0bef5fb32b4f5179add4ba95ad73551505
Gabriel Moreno <Gabriel Moreno@Gabriel>
2018-10-20 20:40:28 -0300

    Pacote util com classe de arredondamento de casas decimais

---
+++ src/util/Arredondamento.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author Gabriel Moreno
+ */
+public class Arredondamento {
+    public void arredonda(double num){
+	int decimalPlace = 2;
+	BigDecimal bd = new BigDecimal(num);
+	bd = bd.setScale(decimalPlace,BigDecimal.ROUND_HALF_UP);
+	num = bd.doubleValue();
+	System.out.println(num);
+    }
+}
+
