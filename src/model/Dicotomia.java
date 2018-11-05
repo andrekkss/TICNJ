90085d85428ab034d9d5a348564f1ffaec9264f3
Jisoo Goddess <37126497+Deruma@users.noreply.github.com>
2018-11-05 21:42:07 -0200

    JavaDoc da classe Dicotomia

--- src/model/Dicotomia.java
+++ src/model/Dicotomia.java
@@ -22,8 +22,16 @@ public class Dicotomia {
     private double pm; private double fpm;
     private double error;
 
+    /**
+     * Objeto instanciado Formulas
+     */
     Formulas f = new Formulas();
 
+    /**
+     * Método que recebe todos os valores necessários para realizar os calculos da Dicotomia
+     * @param x1 recebe o valor X1 do tipo double
+     * @param x2 recebe o valor X2 do tipo double
+     */
     public Dicotomia(double x1, double x2){
         setX1(x1);
         setX2(x2);
@@ -34,6 +42,13 @@ public class Dicotomia {
         setError(getX1(), getX2());
     }
     
+    /**
+     * Método que verifica a condição booleana e retorna a troca das variáveis 
+     * @param x1 parâmetro do tipo double
+     * @param pm ponto médio parâmetro do tipo double
+     * @param fpm função do ponto médio parâmetro do tipo double
+     * @param t parâmetro do tipo boolean 
+     */
     public Dicotomia(double x1, double pm, double fpm, Boolean t){
         if(t){
           setX1(x1);
@@ -52,103 +67,112 @@ public class Dicotomia {
     }
        
     /**
-     * @return the x1
+     * @return retorna x1
      */
     public double getX1() {
         return x1;
     }
 
     /**
-     * @param x1 the x1 to set
+     * @param x1 parâmetro que define o valor de x1
      */
     public void setX1(double x1) {
         this.x1 = x1;
     }
 
     /**
-     * @return the x2
+     * @return retorna x2
      */
     public double getX2() {
         return x2;
     }
 
     /**
-     * @param x2 the x2 to set
+     * @param x2 parâmetro que define o valor de x1
      */
     public void setX2(double x2) {
         this.x2 = x2;
     }
     
     /**
-     * @return the fpm
+     * @return retorna a função do ponto médio
      */
     public double getFpm() {
         return fpm;
     }
 
     /**
-     * @param fpm the fpm to set
+     * @param fpm parâmetro que define o valor da função do ponto médio
      */
     public void setFpm(double fpm) {
         this.fpm = f.fpm(fpm);
     }
 
     /**
-     * @return the fx1
+     * @return retorna função de x1
      */
     public double getFx1() {
         return fx1;
     }
 
     /**
-     * @param fx1 the fx1 to set
+     * @param fx1 define o valor da função x1
      */
     public void setFx1(double fx1) {
         this.fx1 = f.cubo(fx1);
     }
 
     /**
-     * @return the fx2
+     * @return retorna a função de x2
      */
     public double getFx2() {
         return fx2;
     }
 
     /**
-     * @param fx2 the fx2 to set
+     * @param fx2 define o valor da função x2
      */
     public void setFx2(double fx2) {
         this.fx2 = f.cubo(fx2);
     }
 
     /**
-     * @return the pm
+     * @return retorna o ponto médio
      */
     public double getPm() {
         return pm;
     }
 
-    /**
-     * @param pm the pm to set
+    /** 
+     * define o valor do ponto médio
+     * @param x1 parâmetro do tipo double
+     * @param x2 parâmetro do tipo double
      */
     public void setPm(double x1, double x2) {
         this.pm = f.pm(x1, x2);
     }
 
     /**
-     * @return the error
+     * @return retorna o erro
      */
     public double getError() {
         return error;
     }
 
     /**
-     * @param error the error to set
+     * @param error parâmetro que define a margem de erro entre os parâmetros x1 e x2
+     * @param x1 parâmetro do tipo double
+     * @param x2 parâmetro do tipo double
      */
     public void setError(double x1, double x2) {
         this.error = f.margemErro(x1, x2);
     }
     
+    /**
+     * Método que adiciona as colunas f(x1), f(x2), f(pm) e a margem de erro
+     * @param l parâmetro ListM
+     * @return retorna o modelo
+     */
     public static DefaultTableModel getTableModel(ListM l){
         List<Dicotomia> lista = l.getList();
         DefaultTableModel modelo = new DefaultTableModel();
@@ -162,7 +186,11 @@ public class Dicotomia {
         }
         return modelo;
     }
-
+    
+    /**
+     * método toString que retorna todos os valores (x1,x2,fx1,fx2, ponto medio, fpm e a margem de erro)
+     * @return 
+     */
     @Override
     public String toString() {
         return "[x¹: " + getX1() +
